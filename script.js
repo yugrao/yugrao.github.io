8260534b13ae23231968873aa6b561ba92d8db48
Yug Rao <yugr101@gmail.com>
2024-05-12 22:10:56 -0500

    Add blur

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset;
-    var circles = document.querySelectorAll('.circle');
-    var name = document.querySelector('.name');
-
-    circles.forEach(function(circle, index) {
-        var offset = scrollPosition * (index - 1) * 0.3;
-        circle.style.transform = 'translate(' + offset + 'px, -' + scrollPosition + 'px)';
+window.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+    var circles = document.querySelectorAll(".circle");
+    var name = document.querySelector(".name");
+    var circle_cntr = document.querySelector(".circle-container");
+  
+    circles.forEach(function (circle, index) {
+      var offset = scrollPosition * (index - 1) * 0.3;
+      circle.style.transform =
+        "translate(" + offset + "px, -" + scrollPosition + "px)";
     });
-});
+  
+    name.style.transform = "translateY(" + (-18 - scrollPosition) + "px)";
+  
+    circle_cntr.style.setProperty("--blursize", Math.max(50 - (scrollPosition/2), 20) + "px");
+  });
