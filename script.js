73e29506a381d8fb683a515194c74fa7b7b5854a
Yug Rao <yugr101@gmail.com>
2024-05-13 23:17:21 -0500

    Add simple mobile option.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 window.addEventListener("scroll", function () {
-    var scrollPosition = window.scrollY;
-    var circles = document.querySelectorAll(".circle");
-    var name = document.querySelector(".name");
-    var circle_cntr = document.querySelector(".circle-container");
-  
-    circles.forEach(function (circle, index) {
-      var offset = scrollPosition/3 * (index - 1) * 0.3;
+  var scrollPosition = window.scrollY;
+  var circles = document.querySelectorAll(".circle");
+  var name = document.querySelector(".name");
+  var circle_cntr = document.querySelector(".circle-container");
+
+  circles.forEach(function (circle, index) {
+      var offset = scrollPosition / 3 * (index - 1) * 0.3;
       circle.style.transform =
-        "translate(" + offset + "px, -" + scrollPosition/3 + "px)";
-    });
-  
-    name.style.transform = "translateY(" + (-18 - scrollPosition/3) + "px)";
-  
-    circle_cntr.style.setProperty("--blursize", Math.max(50 - (scrollPosition/2), 20) + "px");
+          "translate(" + offset + "px, -" + scrollPosition / 3 + "px)";
   });
+
+  name.style.transform = "translateY(" + (-18 - scrollPosition / 3) + "px)";
+
+  circle_cntr.style.setProperty("--blursize", Math.max(50 - (scrollPosition / 2), 20) + "px");
+});
