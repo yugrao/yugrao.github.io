5ecbc8460c79587d83e685d475702363ff49fb5d
Yug Rao <yugr101@gmail.com>
2024-07-05 14:07:18 -0400

    Edit animation parameters.

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ window.addEventListener("scroll", function () {
   var circle_cntr = document.querySelector(".circle-container");
 
   circles.forEach(function (circle, index) {
-      var offset = scrollPosition / 3 * (index - 1) * 0.3;
+      var offset = scrollPosition / 1.5 * (index - 1) * 0.5;
       circle.style.transform =
           "translate(" + offset + "px, -" + scrollPosition / 3 + "px)";
   });
 
   name.style.transform = "translateY(" + (-18 - scrollPosition / 3) + "px)";
 
-  circle_cntr.style.setProperty("--blursize", Math.max(50 - (scrollPosition / 2), 20) + "px");
+  circle_cntr.style.setProperty("--blursize", Math.max(50 - (scrollPosition / 10), 10) + "px");
 });
